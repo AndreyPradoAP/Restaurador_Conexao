0161a8f61cc77f1c107e2bc4de870681983336e9
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2024-09-04 08:44:35 -0300

    Update verificarIP.sh

--- IPv6/Roteador OpenWRT/verificarIP.sh
+++ IPv6/Roteador OpenWRT/verificarIP.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Esse script verifica de tempos em tempos se o IP da WAN do roteador foi alterado. 
-# Em caso de mudança, o processo de reconexão do túnel é iniciado
+# Script para verificar se houve alteracao no IPv6 dinamico do roteador
+# Caso a mudanca ocorra, eh iniciado o processo de reconexao do tunel
 
-source /etc/flags/localIP.conf
-source /etc/flags/remoteIP.conf
+source /etc/flags/local_wan.conf
+source /etc/flags/remote_wan.conf
 
-# Corrigido para o IPv6
+# Verifico o IPv6 recebido na WAN
 $new_ip = ifconfig eth0 | grep 'inet6 addr: 2' | awk '{print $3}'
 
 if ["$ip_wan" == "$new_ip"]
@@ -14,13 +14,12 @@ then
     exit 0
 fi
 
-echo "IP da WAN alterado. Reestabelecendo comunicação"
-"#local_wan='{$new_ip}'" > ip_addr.txt # Salvo o novo IP no arquivo 
+echo "IP da WAN alterado. Reestabelecendo comunicacao"
+"#local_wan='{$new_ip}'" > ip_addr.txt # Salvo o novo IP no arquivo
 
 # Tento enviar o novo IP da WAN para o servidor por meio do SCP
 do
-    scp local_wan.conf root@$remote_wan:/etc/script/remote_wan.conf
-done while [ $? -eq 1 ]
+    scp /etc/flags/local_wan.conf root@$remote_wan:/etc/flags/remote_wan.condone while [ $? -eq 1 ]
 
 /bin/bash del_connections.sh
 
