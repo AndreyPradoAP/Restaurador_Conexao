0ce3bbb92298973ff812257878006d586e47741c
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2024-09-24 10:43:04 -0300

    Update config_connections.sh
    Att com log funcional

--- IPv6/Server Rocky Linux/config_connections.sh
+++ IPv6/Server Rocky Linux/config_connections.sh
@@ -1,10 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 configCon(){
+        echo "$(date +'%D %H:%M:%S') - Start configCon script" >> /var/log/recon.log
         # Create and enable ip6gretap tunnel
-        ip link add name gretap1 type ip6gretap local xxxxx::xxxx remote $1
-        ip addr add xxx.xxx.xxx.xxx/xx dev gretap1
+        ip link add name gretap1 type ip6gretap local xxxx::xxxx remote "$1"
+        ip addr add 10.0.0.1/30 dev gretap1
         ip link set gretap1 up
 
-        # Create some routes
+        echo "$(date +'%D %H:%M:%S') - new gretap tunnel created with local "$1" remote xxxx::xxxx" >> /var/log/recon.log
+        
+        # Routes for BBU002
+        ip route add 10.37.80.196/32 via 10.0.0.2
+        ip route add 10.37.33.148/32 via 10.0.0.2
+        ip route add 10.37.68.212/32 via 10.0.0.2
+        ip route add 10.100.100.100/32 via 10.0.0.2
+
+        ping "$1" &
+
+        echo "$(date +'%D %H:%M:%S')- Routes create successfull" >> /var/log/recon.log
 }
