685dac71c111785cbb0fe3e635a49397c9406f9b
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2024-09-24 10:41:07 -0300

    Update del_connections.sh
    Att com log funcional

--- IPv6/Server Rocky Linux/del_connections.sh
+++ IPv6/Server Rocky Linux/del_connections.sh
@@ -5,13 +5,17 @@
 # so that new ones can be created in a clean environment
 
 deleteConfigs(){
+        echo "$(date +'%D %H:%M:%S') - Script deleteConfigs start" >> /var/log/recon.log
+
         ip link delete gretap1
         gretap_con=$(ifconfig | grep gretap1 | awk '{print $1}')
-
-        echo "Deletou ai"
-
-        if [ "$gretap_con" = "" ]
+    
+        if [ "$gretap_con" != "" ] 
         then
+                echo "$(date +'%D %H:%M:%S') - Gretap tunnel not deleted" >> /var/log/recon.log
+                echo "$(date +'%D %H:%M:%S') - Reconnection failed\n\n" >> /var/log/recon.log
                 return 0
         fi
+
+        echo "$(date +'%D %H:%M:%S') - Gretap tunnel successfully deleted" >> /var/log/recon.log
 }
