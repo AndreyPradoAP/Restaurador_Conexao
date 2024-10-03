c6bf46bac0f426a00728e514cdb6ceeb0b667918
Andrey Prado de Oliveira <95000214+AndreyPradoAP@users.noreply.github.com>
2024-10-03 09:21:14 -0300

    Create check_commands.sh
    New function for check if commands works

---
+++ IPv6/Server Rocky Linux/check_commands.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+checkCommand() {
+        command=$1
+        reply_ok=$2
+        reply_fail=$3
+
+        while true
+        do
+                $command
+
+                if [ $? -eq 0 ] 
+                then
+                        echo "$(date +'%D %H:%M:%S') - $reply_ok" >> /var/log/recon.log
+                        chmod +x /etc/scripts/verify_IP.sh
+                        break
+                else
+                        echo "$(date +'%D %H:%M:%S') - $reply_fail" >> /var/log/recon.log
+                        chmod -x /etc/scripts/verify_IP.sh
+                        sleep 5
+                fi
+        done        
+}
